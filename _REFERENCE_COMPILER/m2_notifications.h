b376bc9d4e191787790d6526c1c98e6747154528
trijezdci <trijezdci@m2r10.local>
2010-08-21 17:49:55 +0900

    modified notification handler signature

--- _REFERENCE_COMPILER/m2_notifications.h
+++ _REFERENCE_COMPILER/m2_notifications.h
@@ -1,13 +1,13 @@
-/*! Modula-2 R10 Compiler (m2r10c)
+/* Modula-2 R10 Compiler (m2r10c)
  *
- *  @file  m2_notifications.h
- *  @brief Notification handling interface
+ *  m2_notifications.h
+ *  Notification handling interface
  *
- *  @b Author: Benjamin Kowarsch
+ *  Author: Benjamin Kowarsch
  *
- *  @b Copyright: (C) 2010 B.Kowarsch. All rights reserved.
+ *  Copyright (C) 2010 B.Kowarsch. All rights reserved.
  *
- *  @b License:
+ *  License:
  *
  *  Permission is hereby granted to review and test this software for the sole
  *  purpose of supporting the effort by the licensor  to implement a reference
@@ -26,28 +26,60 @@
 #define M2_NOTIFICATIONS_H
 
 
-// FROM common IMPORT cardinal;
+// FROM common IMPORT opaque_t, cardinal;
 #include "common.h"
 
+// FROM m2_filenames IMPORT m2_filename_t;
+#include "m2_filenames.h"
 
-// --------------------------------------------------------------------------
+// FROM m2_fileio IMPORT m2_file_t;
+#include "m2_fileio.h"
+
+
+// ---------------------------------------------------------------------------
 // Notification codes
-// --------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 
 typedef /* m2_notification_t */ enum {
-    M2_NOTIFICATION_ALLOCATION_FAILED,
-    M2_NOTIFICATION_SYNTAX_ERROR
+    M2_NOTIFY_ALLOCATION_FAILED,
+    M2_NOTIFY_ILLEGAL_CHAR_IN_STRING_LITERAL,
+    M2_NOTIFY_SYNTAX_ERROR
     
     // TO DO: add notifications
     
 } m2_notification_t;
 
 
-// --------------------------------------------------------------------------
-// Notification handler type
-// --------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// Notification sources
+// ---------------------------------------------------------------------------
 
-typedef void (*m2_notification_f)(m2_notification_t, const char*, cardinal);
+typedef /* m2_notifier_t */ enum {
+    M2_NOTIFIER_FILEIO,
+    M2_NOTIFIER_LEXER,
+    M2_NOTIFIER_PARSER,
+    M2_NOTIFIER_CODEGEN
+} m2_notifier_t;
+
+
+// ---------------------------------------------------------------------------
+// Notification handler type
+// ---------------------------------------------------------------------------
+//
+// Notification handlers must match the signature of notification handler type
+// <m2_notification_f>.  Notification handler parameters are as follows:
+//
+// o  notification code
+// o  filename to which the notification relates
+// o  file position to which the notification relates
+// o  notifying component that is sending the notification
+// o  arbitrary detail information provided by notifying component
+
+typedef void (*m2_notification_f)(m2_notification_t,
+                                  m2_filename_t,
+                                  m2_file_pos_t,
+                                  m2_notifier_t,
+                                  opaque_t);
 
 
 #endif /* M2_NOTIFICATIONS_H */
