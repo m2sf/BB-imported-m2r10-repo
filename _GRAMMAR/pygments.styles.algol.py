0461bdada40929f8e3b636c853df608656683347
trijezdci <trijezdci@gmail.com>
2015-03-04 17:37:37 +0000

    added styles to accommodate rendering of standard library identifiers

--- _GRAMMAR/pygments.styles.algol.py
+++ _GRAMMAR/pygments.styles.algol.py
@@ -21,11 +21,18 @@ class AlgolStyle(Style):
     styles = {
         Comment:                   "italic #888",
         Comment.Preproc:           "noitalic #888",
+        Comment.Special:           "noitalic #888",
 
         Keyword:                   "bold",
         Keyword.Declaration:       "italic",
 
         Name.Builtin:              "bold italic",
+        Name.Builtin.Pseudo:       "bold italic",
+        Name.Namespace:            "bold italic #666",
+        Name.Class:                "bold italic #666",
+        Name.Function:             "bold italic #666",
+        Name.Variable:             "bold italic #666",
+        Name.Constant:             "bold italic #666",
 
         Operator.Word:             "bold",
 
