9ae71f9c0feb0a175ac2c76408a9f052b8b439f3
trijezdci <trijezdci@gmail.com>
2015-03-07 06:34:16 +0000

    updated docstring, changed to underlining reserved words

--- _GRAMMAR/pygments.styles.algol.py
+++ _GRAMMAR/pygments.styles.algol.py
@@ -3,8 +3,29 @@
     pygments.styles.algol
     ~~~~~~~~~~~~~~~~~~~~~
 
-    Algol publishing style.
-
+    Algol publication style.
+    
+    This style renders source code for publication of algorithms in
+    scientific papers and academic texts, where its format is frequently used.
+    
+    It is based on the style of the revised Algol-60 language report[1].
+    
+    o  No colours, only black, white and shades of grey are used.
+    o  Keywords are rendered in lowercase underline boldface.
+    o  Builtins are rendered in lowercase boldface italic.
+    o  Docstrings and pragmas are rendered in dark grey boldface.
+    o  Library identifiers are rendered in dark grey boldface italic.
+    o  Comments are rendered in grey italic.
+    
+    To render keywords without underlining, refer to the `Algol_Nu` style.
+    
+    For lowercase conversion of keywords and builtins in languages where
+    these are not or might not be lowercase, a supporting lexer is required.
+    The Algol and Modula-2 lexers automatically convert to lowercase whenever
+    this style is selected.
+    
+    [1] `Revised Report on the Algorithmic Language Algol-60 <http://www.masswerk.at/algol60/report.htm>`
+    
     :copyright: Copyright 2006-2015 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
@@ -20,10 +41,10 @@ class AlgolStyle(Style):
 
     styles = {
         Comment:                   "italic #888",
-        Comment.Preproc:           "noitalic #888",
-        Comment.Special:           "noitalic #888",
+        Comment.Preproc:           "bold noitalic #888",
+        Comment.Special:           "bold noitalic #888",
 
-        Keyword:                   "bold",
+        Keyword:                   "underline bold",
         Keyword.Declaration:       "italic",
 
         Name.Builtin:              "bold italic",
@@ -36,7 +57,9 @@ class AlgolStyle(Style):
 
         Operator.Word:             "bold",
 
-        String:                    "italic",
+        String:                    "italic #666",
 
         Error:                     "border:#FF0000"
     }
+
+# end of algol.py
